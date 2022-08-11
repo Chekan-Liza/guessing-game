05101cd3ac11f964ae293bdfa41aecae803fe867
Chekan-Liza <70697985+Chekan-Liza@users.noreply.github.com>
2022-08-11 17:25:35 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor(min, max, border) {
+        this.min = min;
+        this.max = max;
+        this.border = border;
+    }
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
     guess() {
-
+        return this.border = Math.round((this.max - this.min)/2 + this.min);
     }
-
     lower() {
-
+        this.max = this.border;
     }
-
     greater() {
-
+        this.min = this.border;
     }
 }
 
